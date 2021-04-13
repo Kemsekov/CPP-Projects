51da1162b5ff7c3eb404dea115a1332af4970189
Vlad Kemsikov <57869319+Kemsekov@users.noreply.github.com>
2021-04-13 07:51:07 +0300

    Add comments to file.

--- Generator/example.cpp
+++ Generator/example.cpp
@@ -14,25 +14,30 @@ using namespace std;
 int main(int args,char* params[]){
 
 	try{
-
+		//Set random generator seed to current time
 		srand(time(0));
+		//Size of output image. 
 		int size = 0;
+		
 		int randsize = 0;
 
 		if(args==3){
-		stringstream sizestr(params[1]);
-		stringstream randsizestr(params[2]);
+			//read params from program input
+			//--> generator.exe 200 100
+			//will be generated random image with width and height eq to 200px and max range of random eq to 100
+			stringstream sizestr(params[1]);
+			stringstream randsizestr(params[2]);
 
-		sizestr>>size;
-		randsizestr>>randsize;
+			sizestr>>size;
+			randsizestr>>randsize;
 
-		//two underlines defines format of .pgm image
-		ofstream file(string(params[2]).append(".pgm"));
-		file<<"P2"<<'\n'<<size<<' '<<size<<'\n'<<randsize<<'\n';
+			//two underlines defines format of .pgm image
+			ofstream file(string(params[2]).append(".pgm"));
+			file<<"P2"<<'\n'<<size<<' '<<size<<'\n'<<randsize<<'\n';
 
-		Generate_rand(size,randsize,file);
-		file.close();
-		return 0;
+			Generate_rand(size,randsize,file);
+			file.close();
+			return 0;
 		}
 		cout<<"First arg is size, second is the max value for random generator"<<endl;
 		return 1;
